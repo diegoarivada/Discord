9ac006eeb6a0cd92c02a012ae34a8b2a3ced3683
Diego Rivada <71936157+diegoarivada@users.noreply.github.com>
2021-11-20 20:09:34 -0500

    update<working>

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import DiscordJS, { Intents, Message } from 'discord.js'
+import DiscordJS, { Intents, Message, MessageActionRow } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -13,6 +13,56 @@ const client = new DiscordJS.Client({
 client.on('ready', () => {
     console.log('The bot is online')
 })
+/*
+Testing purposes
+({
+    waApi.getFull('sin x').then(console.log).catch(console.error);
+    // { success: true, error: false, numpods: 13, datatypes: '', ...
+    
+    waApi.getFull('F9TVlu5AmVzL').then(console.log).catch(console.error);
+    // { success: false, error: false, numpods: 0, datatypes: '', ...
+    
+    waApi.getFull('sin(x)').then((queryresult) => {
+      const pods = queryresult.pods;
+      const output = pods.maps((pod) => {
+        const subpodContent = pod.subpods.map(subpod =>
+          `  <img src="${subpod.img.src}" alt="${subpod.img.alt}">`
+        ).join('\n');
+        return `<h2>${pod.title}</h2>\n${subpodContent}`;
+      }).join('\n');
+      console.log(output);
+    }).catch(console.error),
+})
+
+
+fetch('https://api.wolframalpha.com/')
+.then(response =>{
+    return response.json();
+}).then(data =>{
+    console.log(data);
+})
+
+< TAKE USER INPUT AS COMMAND<in progress> 
+var inMessage = confirm("Please enter a command");
+if (inMessage == true){
+    x = 'VALID'; 
+    {
+        else {
+    x = 'INVALID';
+    }
+}
+}
+*/
+
+client.on('messageCreate' , (message) => {
+    if (message.content === '!help') {
+        message.reply({
+            content: ' press ping < more in progress> ',
+        })
+    }
+})
+
+
 
 client.on('messageCreate' , (message) => {
     if (message.content === 'ping') {
